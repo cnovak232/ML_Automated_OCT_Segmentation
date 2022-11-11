f6bfe5d25b833adcd7241680cfc8371b827e4082
cnovak232 <117189219+cnovak232@users.noreply.github.com>
2022-11-11 12:52:40 -0500

    Add files via upload

---
+++ MLSP_Project_Driver.m
@@ -0,0 +1,30 @@
+%% Driver file for MLSP Term Project
+% Automatic Segmentatioin of OCT Images
+% Members:
+
+%% Read in Data
+
+% Read in images
+
+% Read in labels
+
+%% Pre process images
+
+% apply filters
+
+%% Extract Segment Features
+
+% Edge/Gradient Detection
+
+% Graph Search
+
+%% Classify Feautres for Segmentation
+
+% Random Forest
+% KNN
+% SVM
+
+
+
+
+

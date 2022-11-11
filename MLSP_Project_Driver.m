3ce5b718d9551fc02a02cb122ba86d1986772a40
Novak <cnovak@github.com>
2022-11-11 14:17:14 -0500

    add image files and quick loop to read in

--- MLSP_Project_Driver.m
+++ MLSP_Project_Driver.m
@@ -3,13 +3,16 @@
 % Members:
 
 %% Read in Data
-
+num_images = 24;
+oct_ims = {};
 % Read in images
+for i = 1:num_images
+    oct_ims{i}= imread('1HOCT_LC266L0_24S.tif',1);
+end
 
 % Read in labels
 
 %% Pre process images
-
 % apply filters
 
 %% Extract Segment Features

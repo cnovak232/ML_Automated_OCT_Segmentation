3d079e4f3d91c20dfb8d35711907abf6a808a53c
Sam Ydenberg <samydenberg@Sams-MBP-9.attlocal.net>
2022-11-20 16:19:50 -0500

    added code for edge and gradient detection

--- MLSP_Project_Driver.m
+++ MLSP_Project_Driver.m
@@ -7,7 +7,7 @@ num_images = 24;
 oct_ims = {};
 % Read in images
 for i = 1:num_images
-    oct_ims{i}= imread('1HOCT_LC266L0_24S.tif',1);
+    oct_ims{i}= imread('./Image_Decks/1HOCT_LC266L0_24S.tif',1);
 end
 
 % Read in labels
@@ -19,6 +19,29 @@ end
 
 % Edge/Gradient Detection
 
+% Edge option
+edge_list = cell(1,size(oct_ims,2));
+
+for i = 1:size(oct_ims,2)
+    sub_imagei = oct_ims{1,i};
+    edge_imagei = edge(sub_image2,'canny',[0.05,0.3],1.7);
+    edge_list{1,i} = edge_imagei;
+    % figure 
+    % imshow(sub_imagei)
+end
+
+% Gradient Option
+magnitude_list = cell(1,size(oct_ims,2));
+direction_list = cell(1,size(oct_ims,2));
+
+for i = 1:size(oct_ims,2)
+    sub_imagei = oct_ims{1,i};
+    [Gmag,Gdir] = imgradient(sub_image2,"prewitt");
+    magnitude_list{1,i} = Gmag;
+    direction_list{1,i} = Gdir;
+%     figure
+%     imshowpair(Gmag,Gdir,'montage')
+end
 % Graph Search
 
 %% Classify Feautres for Segmentation

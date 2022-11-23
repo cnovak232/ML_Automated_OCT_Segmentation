87b00f0b8dce20231088d7ec3ad0d7676b1685cb
Novak <cnovak@github.com>
2022-11-22 19:55:47 -0500

    add spreadsheat, ability to read in markings, sort, and label images

--- MLSP_Project_Driver.m
+++ MLSP_Project_Driver.m
@@ -2,15 +2,37 @@
 % Automatic Segmentatioin of OCT Images
 % Members:
 
-%% Read in Data
-num_images = 24;
-oct_ims = {};
-% Read in images
-for i = 1:num_images
-    oct_ims{i}= imread('./Image_Decks/1HOCT_LC266L0_24S.tif',1);
+%% Read in spreadsheet info
+oct_marks = {};
+filename = './DVC_Marking_Points_10-11-2022.xlsx';
+opt=detectImportOptions(filename);
+shts=sheetnames(filename);
+
+for i=1:numel(shts)
+  oct_marks{i}=readtable(filename,opt,'Sheet',shts(i));
 end
 
-% Read in labels
+%% Select image to process
+lc_num = 'LC471';
+im_name = ['./Image_Decks_wMarks/1HOCT_',lc_num,'L0_24S.tif'];
+
+num_images = 24;
+oct_ims = {};
+% Read in T-Slice images
+for i = 1:num_images
+    oct_ims{i}= imread(im_name,1);
+end
+
+%% Sort labels and images 
+labels = ["bruch_op","bruch_mem_left","bruch_mem_right","ant_lam_lim",...
+    "chor_scl_left","chor_scl_right"];
+
+lc_ind = find(lc_num == shts);
+im_marks = oct_marks{lc_ind};
+
+mark_labels = sort_markings(im_marks,labels);
+
+marked_ims = mark_images(oct_ims,mark_labels);
 
 %% Pre process images
 % apply filters
@@ -48,6 +70,13 @@ end
 
 % Random Forest
 % KNN
+% for l = 1:length(test_labels)
+%     dists = vecnorm( test_weights(:,l) - train_weights);
+%     dists_labeled = [dists', train_labels];
+%     dists_sort = sortrows(dists_labeled);
+%     k_dists = dists_sort(1:Knn(j),:);
+%     knn_test_labels(l) = mode(k_dists(:,2));
+% end
 % SVM
 
 
---
+++ mark_images.m
@@ -0,0 +1,16 @@
+function marked_ims = mark_images(oct_ims,marks)
+marked_ims = {};
+
+for i = 1:length(oct_ims)
+    im = oct_ims{i};
+    im = insertMarker(im,marks(i).bruch_op,'x','Color','red');
+    im = insertMarker(im,marks(i).bruch_mem_left,'*','Color','blue');
+    im = insertMarker(im,marks(i).bruch_mem_right,'*','Color','blue');
+    im = insertMarker(im,marks(i).ant_lam_lim,'*','Color','magenta');
+    im = insertMarker(im,marks(i).chor_scl_left,'*','Color','green');
+    im = insertMarker(im,marks(i).chor_scl_right,'*','Color','green');
+    marked_ims{i} = im;
+end
+
+end
+

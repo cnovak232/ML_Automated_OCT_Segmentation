648f5b7100fbf010e2dc71540793c829a48fbcba
Novak <cnovak@github.com>
2022-11-29 23:49:04 -0500

    partially refactor alg

--- MLSP_Project_Driver.m
+++ MLSP_Project_Driver.m
@@ -19,10 +19,27 @@ im_name = ['./Image_Decks_wMarks/1HOCT_',lc_num,'L0_24S.tif'];
 
 num_images = 24;
 oct_ims = {};
+oct_ims_rs = {};
 % Read in T-Slice images
 for i = 1:num_images
-    oct_ims{i}= imread(im_name,1);
+    oct_ims{i}= imread(im_name,i);
+    oct_ims_rs{i} = imresize(oct_ims{i},1/3);
 end
+
+%% Import all images
+% imdir = './Image_Decks_wMarks';
+% listname = dir(imdir);
+% num_people = 8;
+% num_images = 24;
+% 
+% oct_ims = cell(num_people,num_images);
+% for p = 1:length(listname)
+%     im_file = [imdir listname(p)];
+%     for i = 1:num_images
+%         oct_ims{i,p} = imread(im_file,i);
+%     end
+% end
+
 
 %% Sort labels and images 
 labels = ["bruch_op","bruch_mem_left","bruch_mem_right","ant_lam_lim",...
@@ -58,35 +75,51 @@ tic
 num_feats = 6;
 train_data = [];
 train_labels = [];
-ims_for_class_0 = 1;
+ims_for_class_0 = [1];
 
+resize = 1;
+
+oct = oct_ims;
 % Resizing
-for i = 1:num_images
-    oct_ims{i} = imresize(oct_ims{i},1/3);
+if resize 
+    oct = oct_ims_rs;
 end
 
-for i = 1:4 
-    sub_im = double(oct_ims{i});
+for i = 1:num_images-1
+    sub_im = double(oct{i});
 
-    % compute features and reshape
+    % compute all features and reshape
     [gm,gd] = imgradient(sub_im,'sobel');
+    %W = graydiffweight(sub_im,refGrayVal)
+    W = gradientweight(sub_im);
     avg = imboxfilt(sub_im);
+    [rows,cols] = ndgrid(1:size(sub_im,1),1:size(sub_im,2));
+    rows = reshape(rows,[numel(rows) 1]);
+    cols = reshape(cols,[numel(cols) 1]);
     sz = size(sub_im);
-    sub_im_1d = reshape(sub_im,[1 numel(sub_im)]);
-    gm = reshape(gm,[1 numel(gm)]);
-    gd = reshape(gd,[1 numel(gd)]);
-    avg = reshape(avg,[1 numel(avg)]);
+    sub_im_1d = reshape(sub_im,[numel(sub_im),1]);
+    gm = reshape(gm,[numel(gm) 1]);
+    gd = reshape(gd,[numel(gd) 1]);
+    avg = reshape(avg,[numel(avg) 1]);
+    feats = [sub_im_1d,gm,gd,avg,rows,cols];
+
+    % mean center and normalize
+    mn = mean(feats,1);
+    feats = feats - mn;
+    feats = normc(feats);
 
     % seperate mark locations into feature vectors with labels
     feat_vecs = cell(1,7);
     marks = mark_labels(i);
-    % Resizing marks
-    marks.bruch_op = marks.bruch_op / 3;
-    marks.ant_lam_lim = marks.ant_lam_lim / 3;
-    marks.bruch_mem_left = marks.bruch_mem_left / 3;
-    marks.bruch_mem_right = marks.bruch_mem_right / 3;
-    marks.chor_scl_left = marks.chor_scl_left / 3;
-    marks.chor_scl_right = marks.chor_scl_right / 3;
+    if resize 
+        % Resizing marks
+        marks.bruch_op = marks.bruch_op / 3;
+        marks.ant_lam_lim = marks.ant_lam_lim / 3;
+        marks.bruch_mem_left = marks.bruch_mem_left / 3;
+        marks.bruch_mem_right = marks.bruch_mem_right / 3;
+        marks.chor_scl_left = marks.chor_scl_left / 3;
+        marks.chor_scl_right = marks.chor_scl_right / 3;
+    end
     fn = fieldnames(marks);
     marked_inds = [];
     for f = 1:length(fn)
@@ -94,14 +127,7 @@ for i = 1:4
         rows = inds_2d(:,2);
         cols = inds_2d(:,1);
         inds_1d = sub2ind(sz,rows,cols);
-        feats = zeros(length(inds_1d),6);
-        feats(:,1) = sub_im_1d(inds_1d); % pixel intensity
-        feats(:,2) = gm(inds_1d); % gradient mag
-        feats(:,3) = gd(inds_1d); % gradient dir
-        feats(:,4) = avg(inds_1d); % local average
-        feats(:,5) = rows; % row location   
-        feats(:,6) = cols; % col location
-        feat_vecs{f} = feats;
+        feat_vecs{f} = feats(inds_1d,:);
         train_labels = [train_labels; f*ones(length(inds_1d),1)];
         marked_inds = [marked_inds; inds_1d];
     end
@@ -119,23 +145,15 @@ for i = 1:4
 
         % loop through rest of pixels assigning feature vecs
         idx = 1;
-        feats = zeros(length(sub_im_1d),6); % make this max length for speed
         for p = 1:length(sub_im_1d)
             [row,col] = ind2sub(sz,p);
             if ( any(marked_inds(:) == p ) || row < start_row || sub_im_1d(p) == 0 )
                 continue;
             end
-            feats(idx,1) = sub_im(p); % intensity
-            feats(idx,2) = gm(p); % gradient mag
-            feats(idx,3) = gd(p); % gradient dir
-            feats(idx,4) = avg(p); % local avg
-            feats(idx,5) = row;
-            feats(idx,6) = col;
+            feat_vecs{7}(idx,:) = feats(p,:);
             idx = idx + 1;
         end
-        feats = feats(1:idx,:);
-        feat_vecs{7} = feats;
-        train_labels = [train_labels; zeros(idx,1)];
+        train_labels = [train_labels; zeros(idx-1,1)];
     end
 
     % group all pixel feature vectors from each class/label
@@ -143,11 +161,6 @@ for i = 1:4
     for c = 1:length(feat_vecs)
         all_classes = [all_classes; feat_vecs{c}];
     end
-
-%     Do we want to mean center and normalize the features vecs?
-%     mn = mean(feats(:,1:4),1);
-%     feats(:,1:4) = feats(:,1:4) - mn;
-%     feats(:,1:4) = normc(feats(:,1:4));
 
     train_data = [train_data; all_classes];
 end
@@ -159,6 +172,10 @@ end
 % use T-Slice 24 to test
 im_num = 24;
 test_im = oct_ims{im_num};
+if resize
+    test_im = oct_ims_rs{im_num};
+end
+test_im = double(test_im);
 num_pix = numel(test_im);
 
 % find the main edge to reduce num test pixels
@@ -171,47 +188,44 @@ start_row = min(r_ind) - 1;
 % seperate test im into features
 [gm,gd] = imgradient(test_im,'sobel');
 avg = imboxfilt(test_im);
+[rows,cols] = ndgrid(1:size(sub_im,1),1:size(sub_im,2));
+rows = reshape(rows,[numel(rows) 1]);
+cols = reshape(cols,[numel(cols) 1]);
 sz = size(test_im);
-test_im_1d = reshape(test_im,[1 num_pix]);
-gm = reshape(gm,[1 num_pix]);
-gd = reshape(gd,[1 num_pix]);
-avg = reshape(avg,[1 num_pix]);
-test_feats = zeros(num_pix,6); 
+test_im_1d = reshape(test_im,[num_pix 1]);
+gm = reshape(gm,[num_pix 1]);
+gd = reshape(gd,[num_pix 1]);
+avg = reshape(avg,[num_pix 1]);
+feats = [test_im_1d,gm,gd,avg,rows,cols];
+mn = mean(feats,1);
+feats = feats - mn;
+feats = normc(feats);
+
+test_feats = zeros(size(feats));
 idx = 1;
+label_inds = [];
 for p = 1:num_pix
     [row,col] = ind2sub(sz,p);
     if (row < start_row || test_im_1d(p) == 0 )
         continue;
     end
-    test_feats(idx,1) = test_im_1d(p); % intensity
-    test_feats(idx,2) = gm(p); % gradient mag
-    test_feats(idx,3) = gd(p); % gradient dir
-    test_feats(idx,4) = avg(p); % local avg
-    test_feats(idx,5) = row;
-    test_feats(idx,6) = col;
+    test_feats(idx,: ) = feats(p,:);
+    label_inds = [label_inds; row, col];
     idx=idx+1;
 end
 num_pts = idx;
 test_feats = test_feats(1:num_pts,:);
 
-% Mean center and normalize if done for training data
-% mn = mean(test_feats,1);
-% test_feats = test_feats - mn;
-% test_feats = normc(test_feats);
-
-
 % Run Knn Classification
 knn = 1; % found using only the closest neighbor is best so far
 knn_test_labels = zeros(num_pts,1);
-label_inds = zeros(num_pts,2);
 
 for p = 1:num_pts
     dists = vecnorm( test_feats(p,:) - train_data,2,2);
     dists_labeled = [dists, train_labels];
     [dists_min, ind] = mink(dists,knn);
     k_dists = [dists_min, train_labels(ind) ];
-    knn_test_labels(p) = k_dists(:,2);
-    label_inds(p,:) = test_feats(p,5:6);
+    knn_test_labels(p) = mode(k_dists(:,2));
 end
 
 % Resolve labels and their locations 
@@ -228,47 +242,49 @@ end
 % Convert cell vec to struct for the mark_images() function
 fn = fieldnames(mark_labels(1));
 my_marks = cell2struct(knn_marks,fn,2);
-% Resizing marks
-my_marks.bruch_op = my_marks.bruch_op * 3;
-my_marks.ant_lam_lim = my_marks.ant_lam_lim * 3;
-my_marks.bruch_mem_left = my_marks.bruch_mem_left * 3;
-my_marks.bruch_mem_right = my_marks.bruch_mem_right * 3;
-my_marks.chor_scl_left = my_marks.chor_scl_left * 3;
-my_marks.chor_scl_right = my_marks.chor_scl_right * 3;
-test_im_cell{1} = test_im; % input must be cell array
+if resize 
+    % Resizing marks
+    my_marks.bruch_op = my_marks.bruch_op * 3;
+    my_marks.ant_lam_lim = my_marks.ant_lam_lim * 3;
+    my_marks.bruch_mem_left = my_marks.bruch_mem_left * 3;
+    my_marks.bruch_mem_right = my_marks.bruch_mem_right * 3;
+    my_marks.chor_scl_left = my_marks.chor_scl_left * 3;
+    my_marks.chor_scl_right = my_marks.chor_scl_right * 3;
+end
+test_im_cell{1} = oct_ims{im_num}; % input must be cell array
 % Resize images
-test_im_cell{1} = imresize(test_im_cell{1},3);
+% test_im_cell{1} = imresize(test_im_cell{1},3);
 im_marked = mark_images(test_im_cell,my_marks);
 
 figure;
-subplot(211);
 imshow(marked_ims{24});
 title('Marked from spreadsheet')
-subplot(212);
+figure;
 imshow(im_marked{1});
 title('Marked by algorithm')
 toc
 
 %% SVM Training - Needs testing
 
-%Mdl = fitcecoc(train_data,train_labels);
+Mdl = fitcecoc(train_data,train_labels,"Coding","onevsall");
+%% 
+
+svm_labels = predict(Mdl,test_feats);
 %% Random Forest
 
 %% Extract Segment Features (Experimentation Section)
 
-% % Edge/Gradient Detection
-% 
-% % Edge option
-% edge_list = cell(1,size(oct_ims,2));
-% 
-% for i = 1:size(oct_ims,2)
-%     sub_im = oct_ims{1,i};
-%     edge_imagei = edge(sub_im,'canny',[0.05,0.3],1.7);
-%     edge_list{1,i} = edge_imagei;
-%     % figure 
-%     % imshow(sub_imagei)
-% end
-% 
+% Edge/Gradient Detection
+
+% Edge option
+edge_list = cell(1,size(oct_ims,2));
+
+sub_im = oct_ims{24};
+edge_imagei = edge(sub_im,'sobel',.05);
+edge_list{1,i} = edge_imagei;
+figure;
+imshow(edge_imagei);
+
 % % Gradient Option
 % mag_list = cell(1,size(oct_ims,2));
 % Gdir = cell(1,size(oct_ims,2));

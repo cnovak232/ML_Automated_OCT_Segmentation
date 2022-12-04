6be858c6b2d94156a961062a94c2fe99030e8ce0
Novak <cnovak@github.com>
2022-12-04 00:21:14 -0500

    remove premature masking change

--- ProcessData/computeTrainingDataMask.m
+++ ProcessData/computeTrainingDataMask.m
@@ -13,7 +13,7 @@ for i = 1:length(oct)-1
     
     sub_im_1d= reshape(sub_im,[numel(sub_im),1]);
 
-    [feats, m_inds] = extractFeaturesMasked(sub_im,center_norm,2);
+    [feats, m_inds] = extractFeaturesMasked(sub_im,center_norm);
 
     % seperate mark locations into feature vectors with labels
     marks = mark_labels(i);
--- ProcessData/extractFeaturesMasked.m
+++ ProcessData/extractFeaturesMasked.m
@@ -1,9 +1,5 @@
-function [feats,m_inds] = extractFeaturesMasked(sub_im,center_norm,option)
-    if option == 1
-        mask = imbinarize(sub_im);
-    else
-        mask = gradientEdgeDetection(sub_im,0.3);
-    end
+function [feats,m_inds] = extractFeaturesMasked(sub_im,center_norm)
+    mask = imbinarize(sub_im);
     mask = double(mask);
     mask = reshape(mask,[numel(mask) 1]);
     m_inds = find(mask);
